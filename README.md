6216c0a2d43b7d617fadfd41bcab248f0a72bd2e
Nick Salvatore <nmsalvatore@gmail.com>
2024-09-22 07:55:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn Go With Tests
+
+Personal codebase for the Gitbook [Learn Go With Tests](https://quii.gitbook.io/learn-go-with-tests)
---
+++ hello/hello.go
@@ -0,0 +1,29 @@
+package main
+
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
